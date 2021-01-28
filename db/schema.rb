433344f3cfe21243a0edd0259f7767c0dd58c74c
Colter Ulrich <cju5025@gmail.com>
2021-01-28 11:28:05 -0700

    Adds short_name to cards table, seeds as well

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_045841) do
+ActiveRecord::Schema.define(version: 2021_01_28_180757) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "short_name"
   end
 
 end
